cf0c579d64a85105f129230febab198be9a9272a
merjimenezc <merjimenezc@gmail.com>
2021-11-17 13:42:29 -0800

    Added Insert Into statements to db

--- Library.sql
+++ Library.sql
@@ -13,9 +13,13 @@ DROP TABLE IF EXISTS Ratings;
 CREATE TABLE User (
     u_userkey INT PRIMARY KEY,
     u_name VARCHAR(100) NOT NULL,
-    u_email VARCHARD(100) NOT NULL,
+    u_email VARCHAR(100) NOT NULL,
     u_password VARCHAR(100) NOT NULL
 );
+
+INSERT INTO User(u_userkey, u_name, u_email, u_password) VALUES
+;
+
 
 CREATE TABLE Bookmarks (
     bm_userkey INT PRIMARY KEY,
@@ -26,6 +30,9 @@ CREATE TABLE Bookmarks (
     bm_addDate DATETIME NOT NULL
 );
 
+INSERT INTO Bookmarks(bm_userkey, bm_title, bm_author, bm_publisher, bm_numPages, bm_addDate) VALUES
+;
+
 CREATE TABLE Search (
     s_id INT PRIMARY KEY,
     s_title VARCHAR(100) NOT NULL,
@@ -34,20 +41,33 @@ CREATE TABLE Search (
     s_isbn INT NOT NULL
 );
 
+INSERT INTO Search(s_id, s_title, s_category, s_publisher, s_isbn) VALUES
+;
+
 CREATE TABLE Publisher (
     p_id INT PRIMARY KEY,
     p_name VARCHAR(100) NOT NULL
 );
+
+
+INSERT INTO Publisher(p_id, p_name) VALUES
+;
 
 CREATE TABLE PublisherAuthors (
     p_id INT NOT NULL,
     a_id INT NOT NULL
 );
 
+INSERT INTO PublisherAuthors(p_id, a_id) VALUES
+;
+
 CREATE TABLE Authors (
     a_id INT NOT NULL,
     a_name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO Authors(a_id, a_name) VALUES
+;
 
 CREATE TABLE BookAuthor (
     --FOREIGN KEY(b_id) REFERENCES Book(b_id)
@@ -55,6 +75,9 @@ CREATE TABLE BookAuthor (
     b_id INT NOT NULL,
     a_id INT NOT NULL
 );
+
+INSERT INTO BookAuthor(p_id, p_name) VALUES
+;
 
 CREATE TABLE Book (
     b_id INT PRIMARY KEY,
@@ -67,15 +90,23 @@ CREATE TABLE Book (
     b_publisher VARCHAR(100) NOT NULL
 );
 
+INSERT INTO Book(b_id, b_title, b_rating, b_isbn, b_language, b_numPages, b_publishDate, b_publisher) VALUES
+;
 
 CREATE TABLE Category (
     c_id INT PRIMARY KEY,
     c_name VARCHAR(50) NOT NULL
 );
 
+INSERT INTO Category(c_id, c_name) VALUES
+;
+
 CREATE TABLE Ratings (
     r_id INT PRIMARY KEY,
     r_title VARCHAR(50) NOT NULL,
     r_rating INT(30)
 );
+
+INSERT INTO Ratings(r_id, r_title, r_rating) VALUES
+;
 
